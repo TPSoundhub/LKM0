ba1e986227ad878ccb02562714966e3926ea9db7
TPSoundhub <44570387+TPSoundhub@users.noreply.github.com>
2020-04-02 16:07:26 +0200

    Add files via upload

--- M0Sa-MB-basic-sensor.py
+++ M0Sa-MB-basic-sensor.py
@@ -1,12 +1,14 @@
-# Basic event maker for LYDKit project P1 winter 2019. SSG, SHD, Herningsholm, Skanderborg - sponsered by Region MidtJylland
-# Version 15 oktober 2019, Knud Funch SHD
-# - Test of sensors - temp, magnetometer, light ..
-#
+# M0Sa-MB-basic-sensor.py
+#------------------------
+# Revision 0.3 - 02 apr 2020 - Knud Funch, Soundhub Danmark - LYDKit til undervisningsbrug - Region MidtJylland
 # 
+# - Test of build-in sensors on standalone MB - Accelerometer, temp, magnetometer, light ..
+# - If you want to focus on on sensor just remove some code and modify the rest .. Just try it out.
+# - To make sure you can come back save a copy - but you can always get back as it is on GITHUB
+#
+# See the official MicroPython documentation on: https://microbit-micropython.readthedocs.io/en/latest/
 #
 from microbit import *
-
-
 
 print("Hello World - Klar på den serielle")    # Ready
 display.show(Image.HAPPY)
@@ -15,7 +17,6 @@ sleep(1000)
 display.clear()
 
 while True:                                                                                                          
-
     
      field=compass.get_field_strength()
      mx = compass.get_x()
@@ -24,10 +25,10 @@ while True:
      light=display.read_light_level()
      temp=temperature()
      x,y,z=accelerometer.get_values()
-#     print("")
+     print("")
      print("Magnetic field: ",field, "xyz", mx,my,mz)
-#     print("Ligth: ",light," Temp: ",temp, " Acc xyz: ", x,y,z)
-     sleep(30)
+     print("Ligth: ",light," Temp: ",temp, " Acc xyz: ", x,y,z)
+     sleep(100)  # in milliseconds
              
             
            
